7c9f56c88e4704ecf1ff4ac5b6c12b5885d10aab
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-12-23 16:20:33 +0000

    More mechanics

--- main.cpp
+++ main.cpp
@@ -8,40 +8,100 @@
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_timer.h>
+#include <bits/stdc++.h>
+#include "data.h"
+#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
-#include "data.cpp"
-#include <vector>
 #include <string.h>
+#include <vector>
 using namespace std;
 
 #define global static
 
-typedef struct {
+typedef struct button_struct {
   bool pressed; 
   SDL_FRect rect; 
   const char *name;
+  bool letter = false;
 } button_struct;
+
+typedef struct {
+  SDL_FRect rect; 
+  const char *name;
+} word_struct;
+
+typedef struct gameState {
+  bool running;
+  int currentRound;
+  int totalRounds;
+  bool roundWon;
+  int scoreToWin;
+  int shuffles;
+  int score;
+  bool gameOver;
+} gameState;
 
 global SDL_Window *window = NULL;
 global SDL_Renderer *renderer = NULL;
-global bool running = true;
-
 global int letters_size = 16;
-global int buttons_size = letters_size;
-global vector<button_struct> word;
+global vector<word_struct> wordVector;
+global vector<button_struct> letter_structs;
 global vector<button_struct> button_structs;
-global int score = 0;
+global gameState state {
+  .running = true,
+  .currentRound = 0,
+  .totalRounds = 2,
+  .roundWon = false,
+  .scoreToWin = 6,
+  .shuffles = 3,
+  .score = 0,
+  .gameOver = false
+};
 
-void render_button(SDL_FRect *rect, const char *name, bool pressed) {
-  if (pressed) {
+void render_letters(button_struct button) {
+  if (button.pressed) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
   } else {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   }
 
+  SDL_RenderFillRect(renderer, &button.rect);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  float scale = 4.0f;
+  float text_w = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(button.name);
+  float text_h = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE;
+
+  SDL_SetRenderScale(renderer, scale, scale);
+  float x = (button.rect.x + (button.rect.w - text_w * scale) / 2.0f) / scale;
+  float y = (button.rect.y + (button.rect.h - text_h * scale) / 2.0f) / scale;
+  SDL_RenderDebugText(renderer, x, y, button.name);
+
+  SDL_SetRenderScale(renderer, 1.0f, 1.0f);
+} 
+
+void render_buttons(button_struct button) {
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+  SDL_RenderFillRect(renderer, &button.rect);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  float scale = 4.0f;
+  float text_w = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(button.name);
+  float text_h = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE;
+
+  SDL_SetRenderScale(renderer, scale, scale);
+  float x = (button.rect.x + (button.rect.w - text_w * scale) / 2.0f) / scale;
+  float y = (button.rect.y + (button.rect.h - text_h * scale) / 2.0f) / scale;
+  SDL_RenderDebugText(renderer, x, y, button.name);
+
+  SDL_SetRenderScale(renderer, 1.0f, 1.0f);
+} 
+
+void render_word(SDL_FRect *rect, const char *name) {
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   SDL_RenderFillRect(renderer, rect);
+
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   float scale = 4.0f;
   float text_w = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(name);
@@ -55,18 +115,38 @@ void render_button(SDL_FRect *rect, const char *name, bool pressed) {
   SDL_SetRenderScale(renderer, 1.0f, 1.0f);
 } 
 
-void drawWord() {
-  int index = rand() % 6;
-  const char *random_word = words[index];
+void drawWord(button_struct button) {
+  auto letterInWord = find_if(
+    wordVector.begin(),
+    wordVector.end(),
+    [&](const word_struct &w) {
+    return strcmp(w.name, button.name) == 0;
+    }
+  ); 
 
-  float x = 300;
-  for (int i = 0; i < strlen(random_word); i++) {
-    word.push_back({
-      .pressed = false,
+  if (button.pressed) {
+    float x;
+    printf("word size %zu\n", wordVector.size());
+    if (wordVector.size() > 0) {
+      x = wordVector.back().rect.x + 200;
+    } else {
+      x = 100;
+    }
+    wordVector.push_back({
       .rect = { x, 100, 100, 100 },
-      .name = new char[2] { random_word[i], '\0' } 
+      .name = button.name 
     });
-    x += 200;
+  } else { 
+    if (letterInWord != wordVector.end()) {
+      wordVector.erase(letterInWord);
+    }
+  }
+}
+
+void resetWord() {
+  wordVector.clear();
+  for (int i = 0; i < letter_structs.size(); i++) {
+    letter_structs[i].pressed = false;
   }
 }
 
@@ -80,11 +160,79 @@ void drawScore() {
   SDL_SetRenderScale(renderer, scale, scale);
   float x = 10;
   float y = 10;
-  SDL_RenderDebugTextFormat(renderer, x, y, "SCORE %d", score);
+  SDL_RenderDebugTextFormat(renderer, x, y, "SCORE %d", state.score);
+  SDL_RenderDebugTextFormat(renderer, 100, y, "Round %d/%d", state.currentRound, state.totalRounds);
   SDL_SetRenderScale(renderer, 1.0f, 1.0f);
 }
 
+void drawGameOver() {
+  SDL_RenderClear(renderer);
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  float scale = 4.0f;
+  float text_w = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE; 
+  float text_h = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE;
+
+  SDL_SetRenderScale(renderer, scale, scale);
+  float x = 130;
+  float y = 60;
+  SDL_RenderDebugText(renderer, x, y, "GAME OVER");
+  SDL_RenderDebugTextFormat(renderer, 250, y, "SCORE %d", state.score);
+  SDL_SetRenderScale(renderer, 1.0f, 1.0f);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+
+  button_structs.clear();
+  button_structs.push_back({
+    .pressed = false,
+    .rect = { 400, 400, 300, 150 },
+    .name = "Exit",
+  }); 
+  button_structs.push_back({
+    .pressed = false,
+    .rect = { 800, 400, 300, 150 },
+    .name = "Try again",
+  }); 
+
+  for(int i = 0; i < button_structs.size(); i++) {
+    render_letters(button_structs[i]);
+  }
+}
+
+void drawRoundWon() {
+  SDL_RenderClear(renderer);
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  float scale = 4.0f;
+  float text_w = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE; 
+  float text_h = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE;
+
+  SDL_SetRenderScale(renderer, scale, scale);
+  float x = 130;
+  float y = 60;
+  SDL_RenderDebugText(renderer, x, y, "YOU WON");
+  SDL_RenderDebugTextFormat(renderer, 200, y, "SCORE %d", state.score);
+
+  SDL_SetRenderScale(renderer, 1.0f, 1.0f);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+
+  button_structs.clear();
+  button_structs.push_back({
+    .pressed = false,
+    .rect = { 400, 400, 300, 150 },
+    .name = "Exit",
+  }); 
+  button_structs.push_back({
+    .pressed = false,
+    .rect = { 800, 400, 400, 150 },
+    .name = "Next round",
+  }); 
+
+  for(int i = 0; i < button_structs.size(); i++) {
+    render_letters(button_structs[i]);
+  }
+}
+
 void drawLetters() {
+  letter_structs.clear();
+
   for (int i = 0; i < letters_size; i++) {
     float x;
     float y;
@@ -95,18 +243,87 @@ void drawLetters() {
 
     } else if (i % 4 == 0) {
       x = 600;
-      y = button_structs[i - 1].rect.y + 200;
+      y = letter_structs[i - 1].rect.y + 200;
     } else if (i != 0) {
-      x = button_structs[i - 1].rect.x + 200;
-      y = button_structs[i - 1].rect.y;
+      x = letter_structs[i - 1].rect.x + 200;
+      y = letter_structs[i - 1].rect.y;
     } 
 
-    button_structs.push_back({
+    letter_structs.push_back({
       .pressed = false,
       .rect = { x, y, 100, 100 },
-      .name = new char[2] { letters[rand() % 26], '\0' }
+      .name = new char[2] { letters[rand() % 26], '\0' },
+      .letter = true
     }); 
   }
+}
+
+void shuffle() {
+  resetWord();
+  drawLetters();
+}
+
+void checkRound() {
+  if (state.currentRound >= state.totalRounds) {
+    if (state.score < state.scoreToWin) {
+      state.gameOver = true;
+    } else {
+      state.roundWon = true;
+    }
+  }
+}
+
+void setScore() {
+  string s;
+  for (int i = 0; i < wordVector.size(); i++) {
+    s.append(wordVector[i].name);
+  }
+  auto realWord = find(words.begin(), words.end(), s);
+
+  if (realWord != words.end()){
+    state.score += wordVector.size();
+    int multiplier = 5;
+    int over_5 = wordVector.size() - 5;
+
+    if (over_5 > 0) {
+      state.score += over_5 * multiplier;
+      state.score -= over_5;
+    }
+    shuffle();
+    state.currentRound++;
+    checkRound();
+  }
+}
+
+void drawButtons() {
+  button_structs.push_back({
+    .pressed = false,
+    .rect = { 1450, 700, 300, 150 },
+    .name = "Submit"
+  }); 
+  button_structs.push_back({
+    .pressed = false,
+    .rect = { 1450, 400, 300, 150 },
+    .name = "Reset",
+  }); 
+  button_structs.push_back({
+    .pressed = false,
+    .rect = { 200, 400, 300, 150 },
+    .name = "Shuffle",
+  }); 
+}
+
+void restart() {
+  SDL_RenderClear(renderer);
+  state.score = 0;
+  state.shuffles = 3;
+  state.gameOver = false;
+  state.roundWon = false;
+  state.currentRound = 0;
+  button_structs.clear();
+  drawButtons();
+  resetWord();
+  shuffle();
 }
 
 int main() {
@@ -120,70 +337,79 @@ int main() {
   SDL_RenderClear(renderer);
   SDL_Keycode keycode;
 
-  drawWord();
   drawLetters();
-  button_structs.push_back({
-    .pressed = false,
-    .rect = { 1450, 700, 300, 150 },
-    .name = "Submit"
-  }); 
-  buttons_size++;
+  drawButtons();
 
-  while (running) {
+  while (state.running) {
     SDL_Event event;
     uint64_t PerfCountFrequency = SDL_GetPerformanceFrequency();
     uint64_t LastCounter = SDL_GetPerformanceCounter();
-    drawScore();
+
+    if (state.gameOver) {
+      drawGameOver();
+    } else if (state.roundWon) {
+      drawRoundWon();
+    } else {
+      drawScore();
+      for(int i = 0; i < letter_structs.size(); i++) {
+        render_letters(letter_structs[i]);
+      }
+      for(int i = 0; i < button_structs.size(); i++) {
+        render_buttons(button_structs[i]);
+      }
+      for(int i = 0; i < wordVector.size(); i++) {
+        render_word(&wordVector[i].rect, wordVector[i].name);
+      }
+    }
 
     while (SDL_PollEvent(&event)) {
       switch(event.type) {
         case SDL_EVENT_QUIT:
-          running = false;
+          state.running = false;
           break;
         case SDL_EVENT_MOUSE_BUTTON_DOWN: {
           SDL_FPoint point = { event.button.x, event.button.y };
-          for (int i = 0; i < buttons_size; i++) {
-            button_struct& obj = button_structs[i];
-            
+          for (int i = 0; i < letter_structs.size(); i++) {
+            button_struct& obj = letter_structs[i];
             if (SDL_PointInRectFloat(&point, &obj.rect)) {
               if (obj.pressed) {
                 obj.pressed = false;
+                drawWord(obj);
               } else {
                 obj.pressed = true;
-                if (strcmp(obj.name, "Submit") == 0) {
-                  score++;
-                }
+                drawWord(obj);
+              }
+            }
+          }
+
+          for (int i = 0; i < button_structs.size(); i++) {
+            button_struct& obj = button_structs[i];
+            if (SDL_PointInRectFloat(&point, &obj.rect)) {
+              if (strcmp(obj.name, "Submit") == 0) {
+                setScore();
+              } 
+              if (strcmp(obj.name, "Reset") == 0) {
+                resetWord();
+              }
+              if (strcmp(obj.name, "Shuffle") == 0) {
+                shuffle();
+              }
+              if (strcmp(obj.name, "Exit") == 0) {
+                state.running = false;
+              }
+              if (strcmp(obj.name, "Try again") == 0) {
+                restart();
+              }
+              if (strcmp(obj.name, "Next round") == 0) {
+                restart();
               }
             }
           }
           break;
         }
-        case SDL_EVENT_KEY_DOWN:
-          keycode = event.key.key;
-          switch(keycode) {
-            case SDLK_W:
-              break;
-            case SDLK_S:
-              break;
-            case SDLK_D:
-              break;
-            case SDLK_A:
-              break;
-          }
-          break;
       }
     }
-    for(int i = 0; i < word.size(); i++) {
-      render_button(&word[i].rect, word[i].name, word[i].pressed);
-    }
 
-    for(int i = 0; i < buttons_size; i++) {
-      render_button(
-        &button_structs[i].rect,
-        button_structs[i].name,
-        button_structs[i].pressed
-      );
-    }
     SDL_RenderPresent(renderer);
 
     uint64_t EndCounter = SDL_GetPerformanceCounter();
@@ -193,6 +419,7 @@ int main() {
     double FPS = (double)PerfCountFrequency / (double)CounterElapsed;
 
     // printf("%.02f ms/f, FPS: %.02f/s\n", MSPerFrame, FPS);
+    SDL_Delay(16);
     LastCounter = EndCounter;
   }
 
